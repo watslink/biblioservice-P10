f8394f83ef76b42c47ddba25646a2687fbaedb70
watslink <seb.decroix@gmail.com>
2019-11-12 16:32:26 +0100

    annotation RG

--- biblioservice-business/src/main/java/com/sd/oc/Service/ServiceImpl/ReservationServiceImpl.java
+++ biblioservice-business/src/main/java/com/sd/oc/Service/ServiceImpl/ReservationServiceImpl.java
@@ -1,7 +1,6 @@
 package com.sd.oc.Service.ServiceImpl;
 
 import com.sd.oc.DAO.ReservationDAO;
-import com.sd.oc.Service.ServiceInterface.BookService;
 import com.sd.oc.Service.ServiceInterface.ReservationService;
 import com.sd.oc.model.Book;
 import com.sd.oc.model.Borrowing;
@@ -10,12 +9,13 @@ import com.sd.oc.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ReservationServiceImpl implements ReservationService {
 
 
@@ -39,8 +39,11 @@ public class ReservationServiceImpl implements ReservationService {
     public void addReservationBorrowing(Book book, User user) {
         boolean AlreadyBorrow= false;
         if(book.getNbStock() ==0){
+            //RG:La liste de réservation ne peut comporter qu’un maximum de personnes correspondant à 2x
+            // le nombre d’exemplaires de l’ouvrage.
             if(reservationDAO.findByBookOrderByDateReservation(book).size() < 2*book.getNbTotalExemplaire()){
                 for(Borrowing borrowing : user.getListOfBorrowings()){
+                    //RG:Il n’est pas possible pour un usager de réserver un ouvrage qu’il a déjà en cours d’emprunt
                     if(borrowing.getBook().equals(book)){
                         AlreadyBorrow = true;
                     }
