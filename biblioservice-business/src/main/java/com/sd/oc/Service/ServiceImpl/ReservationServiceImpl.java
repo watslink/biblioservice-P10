18ee9784d65ee346f4afb262b0820780f1a961a3
watslink <seb.decroix@gmail.com>
2019-11-14 16:38:18 +0100

    isAfter to isBefore in deleteAllReservationOutOfDate

--- biblioservice-business/src/main/java/com/sd/oc/Service/ServiceImpl/ReservationServiceImpl.java
+++ biblioservice-business/src/main/java/com/sd/oc/Service/ServiceImpl/ReservationServiceImpl.java
@@ -88,7 +88,7 @@ public class ReservationServiceImpl implements ReservationService {
         List<Reservation> reservationList = (List<Reservation>)reservationDAO.findAll();
         for(Reservation resa: reservationList){
             if(resa.getDateStartMailing() != null){
-                if(resa.getDateStartMailing().plus(2, ChronoUnit.DAYS).isAfter(LocalDate.now())){
+                if(resa.getDateStartMailing().plus(2, ChronoUnit.DAYS).isBefore(LocalDate.now())){
                     reservationDAO.delete(resa);
                 }
             }
